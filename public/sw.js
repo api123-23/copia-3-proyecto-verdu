575fa93420a5dd3a1e48cf9d10399b284270f34d
ignacioricciutti <ricciuttiignacio@gmail.com>
2026-09-09 12:49:15 -0300

    cambios en las pliticas parte 2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "verdu-shell-5YNJSOEhQEddx-rV838W-";
+const CACHE = "verdu-shell--fIf3RlanIyDc3yWDqVZx";
 const PRECACHE = [
   "/",
   "/login",
@@ -6,9 +6,9 @@ const PRECACHE = [
   "/icons/icon-180.png",
   "/icons/icon-192.png",
   "/icons/icon-512.png",
-  "/_next/static/5YNJSOEhQEddx-rV838W-/_buildManifest.js",
-  "/_next/static/5YNJSOEhQEddx-rV838W-/_clientMiddlewareManifest.js",
-  "/_next/static/5YNJSOEhQEddx-rV838W-/_ssgManifest.js",
+  "/_next/static/-fIf3RlanIyDc3yWDqVZx/_buildManifest.js",
+  "/_next/static/-fIf3RlanIyDc3yWDqVZx/_clientMiddlewareManifest.js",
+  "/_next/static/-fIf3RlanIyDc3yWDqVZx/_ssgManifest.js",
   "/_next/static/chunks/02fh7_m5mrih8.js",
   "/_next/static/chunks/07nldpx3i6mc_.js",
   "/_next/static/chunks/0cz1d0mv5g_q7.js",
--- supabase/schema.sql
+++ supabase/schema.sql
@@ -634,11 +634,11 @@ create policy informes_select on informes_generales for select to authenticated
   using (public.es_admin() or (tecnico_id = auth.uid() and estado_firma <> 'firmado'));
 drop policy if exists informes_insert on informes_generales;
 create policy informes_insert on informes_generales for insert to authenticated
-  with check (public.es_admin() or tecnico_id = auth.uid());
+  with check (true);
 drop policy if exists informes_update on informes_generales;
 create policy informes_update on informes_generales for update to authenticated
-  using (public.es_admin() or tecnico_id = auth.uid())
-  with check (public.es_admin() or tecnico_id = auth.uid());
+  using (true)
+  with check (true);
 drop policy if exists informes_delete_admin on informes_generales;
 create policy informes_delete_admin on informes_generales for delete to authenticated
   using (true);
