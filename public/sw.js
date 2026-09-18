055175d3a03c8c1feae4c081543698294e09d441
ignacioricciutti <ricciuttiignacio@gmail.com>
2026-09-18 09:21:13 -0300

    solucion popup firmas y cambios visuales

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "verdu-shell-KysI6cQeWu840R1VZSXlQ";
+const CACHE = "verdu-shell-AxxNlSl5yaML_0DpGvwhY";
 const PRECACHE = [
   "/",
   "/login",
@@ -6,17 +6,17 @@ const PRECACHE = [
   "/icons/icon-180.png",
   "/icons/icon-192.png",
   "/icons/icon-512.png",
-  "/_next/static/KysI6cQeWu840R1VZSXlQ/_buildManifest.js",
-  "/_next/static/KysI6cQeWu840R1VZSXlQ/_clientMiddlewareManifest.js",
-  "/_next/static/KysI6cQeWu840R1VZSXlQ/_ssgManifest.js",
+  "/_next/static/AxxNlSl5yaML_0DpGvwhY/_buildManifest.js",
+  "/_next/static/AxxNlSl5yaML_0DpGvwhY/_clientMiddlewareManifest.js",
+  "/_next/static/AxxNlSl5yaML_0DpGvwhY/_ssgManifest.js",
   "/_next/static/chunks/02fh7_m5mrih8.js",
+  "/_next/static/chunks/042jdir86df-w.css",
   "/_next/static/chunks/07nldpx3i6mc_.js",
   "/_next/static/chunks/0_xmaw59m1r6s.js",
   "/_next/static/chunks/0cz1d0mv5g_q7.js",
   "/_next/static/chunks/0k16m1c57o-qb.js",
   "/_next/static/chunks/16ihqb68jjg31.js",
   "/_next/static/chunks/1cwczo7gh-yho.js",
-  "/_next/static/chunks/1dx0icitvkeef.css",
   "/_next/static/chunks/1kden681vlcis.js",
   "/_next/static/chunks/1vmuvg71dkxre.js",
   "/_next/static/chunks/2-cytfxc_rgaj.js",
